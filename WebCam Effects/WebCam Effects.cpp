9f09dd6cdd75ecb95af311caece02e6b198f591c
Thomas Wiederkehr <thomas-611@hotmail.com>
2024-01-09 01:22:06 -0300

    last version

--- WebCam Effects/WebCam Effects.cpp
+++ WebCam Effects/WebCam Effects.cpp
@@ -1,13 +1,10 @@
 #include <opencv2/opencv.hpp>
-#define D_CONTRAST 50
-#define D_BRIGHT 127
+#include <fstream>
+#define D_CONTRAST 30
 
 using namespace cv;
 
-static void on_trackbar(int, void*)
-{
-	
-}
+static void on_trackbar(int, void*){}
 
 int main(int argc, char** argv)
 {
@@ -17,17 +14,28 @@ int main(int argc, char** argv)
 		return 0;
 
 	namedWindow("Original Image", WINDOW_AUTOSIZE);
-	int alpha_slider = 0;
-	createTrackbar("Trackbar", "Original Image", &alpha_slider, 255, on_trackbar);
+	int blurIntensity = 0, brightness=0, contrast=0;
 	Mat srcframe, dstframe;
-	char key=' ', effect=' ';
+	char key=' ', effect=' ', lastkey=' ';
 	Size blur(0,0);				// Gaussian Bluer Kernel
-	double filters[11] = {0};	// Array to check wich filters are activated 
+	int filters[11] = {0};		// Array to check wich filters are activated 
 								// (if contrast and brightness are higher than zero, 
-	filters[4] = D_CONTRAST;
-
 	VideoWriter video;
 	int record=0;
+
+	std::cout << "Functions and Filters:\n"
+		<< "- L or l: Low-Pass filter (Gaussian Blur)\n"
+		<< "- E or e: Edge detection filter (Canny)\n"
+		<< "- S or s: Sobel filter\n"
+		<< "- B or b: Brightness adjustment\n"
+		<< "- C or c: Contrast adjustment\n"
+		<< "- N or n: Negative filter\n"
+		<< "- G or g: Grayscale filter\n"
+		<< "- O or o: Zoom Out (2x)\n"
+		<< "- T or t: Turn right (90 degrees rotation)\n"
+		<< "- H or h: Horizontal flip\n"
+		<< "- V or v: Vertical flip\n"
+		<< "- R or r: Record video (1 click starts 2 clicks stops)\n";
 
 	for (;;)
 	{
@@ -35,12 +43,6 @@ int main(int argc, char** argv)
 		if (srcframe.empty()) 
 			break; // end of video stream
 		cap >> dstframe;
-
-		Size kernelSize(0, 0);
-		if (alpha_slider % 2 == 0)	
-			kernelSize = Size(alpha_slider+1, alpha_slider+1);
-		else						
-			kernelSize = Size(alpha_slider, alpha_slider);
 
 		key = waitKey(1);
 		if (key == 27)	// stop capturing by pressing ESC
@@ -52,71 +54,111 @@ int main(int argc, char** argv)
 		switch(toupper(effect)) {
 			case 'L':
 				filters[0] = 1;
-				blur = kernelSize;
+				if (filters[0] && lastkey!=effect) {
+					destroyWindow("Original Image");
+					namedWindow("Original Image", WINDOW_AUTOSIZE);
+					createTrackbar("Blur", "Original Image", &blurIntensity, 100, on_trackbar);
+				}
+				lastkey = effect;
 				break;
 			
 			case 'E':
 				filters[2] = 0;
 				filters[1] = !filters[1];
-				effect = ' ';
 				break;
 
 			case 'S':
 				filters[1] = 0;
 				filters[2] = !filters[2];
-				effect = ' ';
 				break;
 
 			case 'B':
-				filters[3] = alpha_slider- D_BRIGHT;
+				if (!filters[3] && lastkey != effect) {
+					destroyWindow("Original Image");
+					namedWindow("Original Image", WINDOW_AUTOSIZE);
+					createTrackbar("Brightness", "Original Image", &brightness, 255, on_trackbar);
+					setTrackbarMin("Brightness", "Original Image", -255);
+					setTrackbarPos("Brightness", "Original Image", 0);
+				}
+				else if (filters[3] && lastkey != effect) {
+					destroyWindow("Original Image");
+					namedWindow("Original Image", WINDOW_AUTOSIZE);
+					createTrackbar("Brightness", "Original Image", &brightness, 255, on_trackbar);
+					setTrackbarMin("Brightness", "Original Image", -255);
+					setTrackbarPos("Brightness", "Original Image", brightness);
+				}
+				lastkey = effect;
+				filters[3] = 1;
 				break;
 
 			case 'C':
-				filters[4] = (double) alpha_slider/ D_CONTRAST;
+				if (!filters[4] && lastkey != effect) {
+					destroyWindow("Original Image");
+					namedWindow("Original Image", WINDOW_AUTOSIZE);
+					createTrackbar("Contrast", "Original Image", &contrast, 100, on_trackbar);
+					setTrackbarPos("Contrast", "Original Image", D_CONTRAST);
+				}
+				else if (filters[4] && lastkey != effect){
+					destroyWindow("Original Image");
+					namedWindow("Original Image", WINDOW_AUTOSIZE);
+					createTrackbar("Contrast", "Original Image", &contrast, 100, on_trackbar);
+				}
+				lastkey = effect;
+				filters[4] = 1;
 				break;
 
 			case 'N':
 				filters[5] = !filters[5];
-				effect = ' ';
 				break;
 
 			case 'G':
 				filters[6] = !filters[6];
-				effect = ' ';
 				break;
 
 			case 'O':
 				filters[7] = !filters[7];
-				effect = ' ';
 				break;
 
 			case 'T':
 				filters[8] +=1;
 				if (filters[8] == 4) filters[8] = 0;
-				effect = ' ';
 				break;
 
 			case 'H':
 				filters[9] = !filters[9];
-				effect = ' ';
 				break;
 
 			case 'V':
 				filters[10] = !filters[10];
-				effect = ' ';
 				break;
 
 			case 'R':
 				if (record)
 					video.release();			//finish video recording
 				else {
-					video = VideoWriter("webcamvideo.mp4", VideoWriter::fourcc('m', 'p', '4', 'v'), 10, Size(dstframe.cols, dstframe.rows), true);
+					std::string filename = "webcamvideo.mp4";
+					std::ifstream f(filename);
+					int i = 2;
+					while (f.good()) {
+						filename = "webcamvideo"+std::to_string(i);
+						filename = filename + ".mp4";
+						f = std::ifstream(filename);
+						i++;
+					}
+					video = VideoWriter(filename, VideoWriter::fourcc('m', 'p', '4', 'v'), 10, Size(dstframe.cols, dstframe.rows), true);
 				}
 				record = !record;
-				effect = ' ';
 				break;
 
 		}
+
+		effect = ' ';
+
+		if (blurIntensity % 2 == 0)
+			blur = Size(blurIntensity + 1, blurIntensity + 1);
+		else
+			blur = Size(blurIntensity, blurIntensity);
+
 
 		if (filters[7])
 			resize(dstframe,dstframe,Size(dstframe.cols /2,dstframe.rows/2));
@@ -125,10 +167,10 @@ int main(int argc, char** argv)
 			GaussianBlur(dstframe, dstframe, blur, 0);
 		
 		if (filters[3])
-			dstframe.convertTo(dstframe, 16, 1, filters[3]);
+			dstframe.convertTo(dstframe, 16, 1, brightness);
 
-		if (filters[4]!= D_CONTRAST)
-			dstframe.convertTo(dstframe, 16, filters[4], 0);
+		if (filters[4])
+			dstframe.convertTo(dstframe, 16, (double) contrast/D_CONTRAST, 0);
 
 		if (filters[5])
 			dstframe.convertTo(dstframe, 16, -1, 255);
@@ -149,7 +191,7 @@ int main(int argc, char** argv)
 		}
 		else if (filters[2]) {
 			cvtColor(dstframe, dstframe, COLOR_BGR2GRAY);
-			Sobel(dstframe, dstframe, 0, 1, 1, 5);
+			Sobel(dstframe, dstframe, -1, 1, 1, 5);
 			cvtColor(dstframe, dstframe, COLOR_GRAY2BGR);
 		}
 		else if (filters[6]) {
